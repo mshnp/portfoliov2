c13edba2e14973f8040773f099c8256663580090
mishean <mishean.jp@hotmail.com>
2022-12-28 02:29:37 -0500

    added red for errors in sub and underline for head

--- src/components/Form.js
+++ src/components/Form.js
@@ -31,7 +31,7 @@ const Form = () => {
 
 
     const { register, handleSubmit, formState: { errors, isValid, isSubmitting }, reset } = useForm({
-        mode: 'all', resolver: yupResolver(schema), criteriaMode: "all"
+        mode: 'onBlur', resolver: yupResolver(schema), criteriaMode: "all"
       });
 
     
@@ -59,16 +59,19 @@ const Form = () => {
   <h2 className="text-center pb-2.5">{submitMessage}</h2>
    <form className="flex flex-col items-center" onSubmit={handleSubmit(_handleSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-      <input className="w-[300px] sm:w-2/3 lg:w-1/3 uppercase text-center appearance-none border rounded leading-tight focus:outline-none focus:shadow-outline" placeholder="name" value={mailchimp.FNAME} {...register("FNAME")}  name="FNAME" onChange={_handleChange}/>
+      <input className={`w-[300px] 
+      ${errors.FNAME
+                ? "border-red-400" : "border-black"} sm:w-2/3 lg:w-1/3 uppercase text-center appearance-none border rounded leading-tight focus:outline-none focus:shadow-outline`} placeholder="name" value={mailchimp.FNAME} {...register("FNAME")}  name="FNAME" onChange={_handleChange}/>
         <p className="text-center text-red-700 py-[5px]">
             {errors.FNAME?.message}
         </p>
       {/* include validation with required or other standard HTML validation rules */}
-      <input className="w-[300px] sm:w-2/3 lg:w-1/3 uppercase text-center appearance-none border rounded leading-tight focus:outline-none focus:shadow-outline" placeholder='email' value={mailchimp.EMAIL} {...register("EMAIL")} name="EMAIL" onChange={_handleChange}/>
+      <input className={`w-[300px] ${errors.EMAIL
+                ? "border-red-400" : "border-black"} sm:w-2/3 lg:w-1/3 uppercase text-center appearance-none border rounded leading-tight focus:outline-none focus:shadow-outline`} placeholder='email' value={mailchimp.EMAIL} {...register("EMAIL")} name="EMAIL" onChange={_handleChange}/>
      
       <p className="py-[5px] text-center text-red-700">{errors.EMAIL?.message}</p>
     
-<button className="uppercase text- mb-2.5 cursor-pointer bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" disabled={!isValid}>
+<button className="uppercase text- mb-2.5 cursor-pointer bg-black disabled:bg-slate-400 active:bg-yellow-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
     submit
 </button>
     </form>
--- src/components/Header.js
+++ src/components/Header.js
@@ -16,16 +16,16 @@ const Header = () => {
    <header className='sticky top-0 z-50 bg-white'>
     <div className='flex text-2xl items-center justify-between h-16 relative max-w-6xl lg:mx-auto'>
     {/* left */}
-    <Link to='/' className='ml-5 select-none font-semibold w-36' onClick={handleClose}> 
+    <Link to='/' className='ml-5 hover:underline select-none font-semibold w-36' onClick={handleClose}> 
         Mishean
     </Link>
 
-    <Link to='/work' className='font-semibold hidden md:flex justify-center'> 
+    <Link to='/work' className='hover:underline font-semibold hidden md:flex justify-center'> 
         Work
     </Link>
         
      {/* right */}
-    <Link to='/subscribe' className='font-semibold hidden md:flex w-36 mr-5'> 
+    <Link to='/subscribe' className='hover:underline font-semibold hidden md:flex w-36 mr-5'> 
         Subscribe
     </Link>
     
@@ -38,8 +38,8 @@ const Header = () => {
     </div>
     </header>
     <div className={nav ? 'overflow-y-scroll justify-center z-10 md:hidden ease-in duration-300 fixed top-0 bottom-0 left-0 right-0 bg-white/90 w-screen flex flex-col items-center': 'flex fixed top-0 h-screen left-[-100%] ease-in duration-300'}>
-            <Link className='select-none p-5 text-xl font-semibold'  to='/work' onClick={handleClick}>Work</Link>
-            <Link className='select-none p-5 text-xl font-semibold' to='/subscribe' onClick={handleClick}>Subscribe</Link>
+            <Link className='select-none hover:underline p-5 text-xl font-semibold'  to='/work' onClick={handleClick}>Work</Link>
+            <Link className='select-none hover:underline p-5 text-xl font-semibold' to='/subscribe' onClick={handleClick}>Subscribe</Link>
     </div> 
    </>
   )
