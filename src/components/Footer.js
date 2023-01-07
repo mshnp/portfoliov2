bd404dbfe33c0aa385ace1772858fa763b5aee26
mishean <mishean.jp@hotmail.com>
2023-01-07 15:29:21 -0500

    changed github username

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className="sticky top-[100vh] h-16 my-5 flex items-center justify-center">
       <div className='flex flex-col items-center justify-center text-lg w-[300px]'>
         <div className='flex flex-row w-[100px] items-center justify-between mb-1'>
-        <a href='https://github.com/mishean' target="_blank"><FaGithub className='text-[20px]'/></a>
+        <a href='https://github.com/mshnp' target="_blank"><FaGithub className='text-[20px]'/></a>
         <a href='https://www.are.na/00015-p' target='_blank' className='text-[20px]'>✶✶</a>
         <a href='mailto:m23006965@proton.me'><EnvelopeIcon className='h-[20px]'/></a>
         </div>
